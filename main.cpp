bd35aa12468e7284573f04e93e5c6ad10958b814
SantiagoReyes2819 <regs090628hgtynna8@cbtis217.edu.mx>
2025-11-24 01:53:15 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
+// programa para saber cuantos numeros 8 hay
+
 #include <iostream>
 using namespace std;
 
 int main() {
+    // Cantidad de numeros 
     int n, x;
-    cin >> n;
+    // Lecturas de los numeros 
+    cin >> n;           
+
+    
     int hay;
     for (int i = 0; i < n; i++) {
         cin >> x;
         if (x == 8) {
+            // si el numero es 8 lo marca 
             cout<<"1";
             return 0;
         }
